376d54286c78cc9f3478f6929b7781aa3d9e972c
supmcgangee <41236072+supmcgangee@users.noreply.github.com>
2020-08-27 13:41:14 +0100

    Added WebAPI client Nuget Package

--- APIRequester.cs
+++ APIRequester.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 
 namespace DeptTechExercise
 {
     public class APIRequester
     {
+        private HttpClient client;
+
+        public APIRequester()
+        {
+            client = new HttpClient();
+        }
     }
 }
