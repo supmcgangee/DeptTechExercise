c68cc7b93b979643e2b1cc812a297c3779cd0486
supmcgangee <41236072+supmcgangee@users.noreply.github.com>
2020-08-27 15:36:05 +0100

    Models created

---
+++ Models/CityResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeptTechExercise.Models
+{
+    public class CityResponseModel
+    {
+        public string locationId;
+        public string cityName;
+        public string country;
+        public List<MeasurementModel> measurements;
+    }
+}
---
+++ Models/MeasurementModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeptTechExercise.Models
+{
+    public class MeasurementModel
+    {
+        public string parameter;
+        public double value;
+        public string lastUpdated;
+        public string unit;
+        public string sourceName;
+    }
+}
